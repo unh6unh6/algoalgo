bf7d787128cabbd5ae6880022a61eaadb0825267
민경윤 <144558971+unh6unh6@users.noreply.github.com>
2026-01-21 18:04:44 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 46212 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder result = new StringBuilder();
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        func(0, 0, new LinkedHashSet<>());
+
+        System.out.println(result);
+    }
+
+    static void func(
+            int before,
+            int cnt,
+            LinkedHashSet<Integer> set
+    ) {
+        if (cnt == m) {
+            for (final Integer i : set) {
+                result.append(i).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (set.contains(i)) {
+                continue;
+            }
+            set.add(i);
+            func(i, cnt + 1, set);
+            set.remove(i);
+        }
+    }
+}
